252fae24c6e86b9ed00b8ceadeacdd4fbd081d90
machangchang <2689920998@qq.com>
2019-04-07 14:48:10 +0800

    配置服务中心提交

---
+++ config-server/src/main/java/com/spring/cloud/configserver/ConfigserverApplication.java
@@ -0,0 +1,19 @@
+package com.spring.cloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@RefreshScope
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
