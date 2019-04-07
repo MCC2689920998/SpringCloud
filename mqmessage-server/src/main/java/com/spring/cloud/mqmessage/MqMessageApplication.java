daf1bfe56d7fdd0c42180e1698a7b3493a2465a4
machangchang <2689920998@qq.com>
2019-04-08 00:33:44 +0800

    注册中心提交

---
+++ mqmessage-server/src/main/java/com/spring/cloud/mqmessage/MqMessageApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.mqmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqMessageApplication.class, args);
+	}
+
+}
