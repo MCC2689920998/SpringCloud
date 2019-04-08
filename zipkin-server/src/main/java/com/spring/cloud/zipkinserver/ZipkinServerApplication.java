f8010f61999d07eb0f3f65618b5997489e45edbb
machangchang <2689920998@qq.com>
2019-04-08 18:46:05 +0800

    注册中心提交

--- zipkin-server/src/main/java/com/spring/cloud/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/spring/cloud/zipkinserver/ZipkinServerApplication.java
@@ -3,10 +3,8 @@ package com.spring.cloud.zipkinserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import zipkin2.server.internal.EnableZipkinServer;
 
 @EnableEurekaClient
-@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinServerApplication {
 
